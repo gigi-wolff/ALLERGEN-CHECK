ebb4c6c890d650bf629d941b8406da107c6da149
Gigi Wolff <gigi.wolff@gmail.com>
2018-07-07 23:21:07 -0400

    search on product.name

--- app/models/search_all_products.rb
+++ app/models/search_all_products.rb
@@ -2,7 +2,7 @@ def search_all_products(search_item)
   # LIKE is the SQL standard while ILIKE is a useful extension made by PostgreSQL.
   # use ILIKE in production and LIKE in development
 
-  return Product.where "name ILIKE ? OR ingredients ILIKE ?", "%#{search_item}%", "%#{search_item}%"
-  #return Product.where "name LIKE ? OR ingredients LIKE ?", "%#{search_item}%", "%#{search_item}%"
+  #return Product.where "name ILIKE ? OR ingredients ILIKE ?", "%#{search_item}%", "%#{search_item}%"
+  return Product.where "name LIKE ? OR ingredients LIKE ?", "%#{search_item}%", "%#{search_item}%"
 
 end
