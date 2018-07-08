8705e0cf032e5cdc37c281e3d45aca5881f4d8a5
Gigi Wolff <gigi.wolff@gmail.com>
2018-07-07 23:23:03 -0400

    changed LIKE to ILIKE

--- app/models/search_all_allergens.rb
+++ app/models/search_all_allergens.rb
@@ -1,7 +1,7 @@
 def search_all_allergens(search_item)
   # LIKE is the SQL standard while ILIKE is a useful extension made by PostgreSQL.
   # use ILIKE in production and LIKE in development
-  #return Allergen.where "category ILIKE ? OR substances ILIKE ?", "%#{search_item}%", "%#{search_item}%"
-  return Allergen.where "category LIKE ? OR substances LIKE ?", "%#{search_item}%", "%#{search_item}%"
+  return Allergen.where "category ILIKE ? OR substances ILIKE ?", "%#{search_item}%", "%#{search_item}%"
+  #return Allergen.where "category LIKE ? OR substances LIKE ?", "%#{search_item}%", "%#{search_item}%"
 
 end
